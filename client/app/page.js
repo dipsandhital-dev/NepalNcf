ff80e19a20a38de79ad4a10c244de3108ba67a97
Dipsan Dhital <dipsandhital12@gmail.com>
2026-02-12 07:49:49 +0545

    Settingup the project

--- client/app/page.js
+++ client/app/page.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const HomePage = () => {
   return (
-    <>
-    
-    
-    </>
+    <div>
+      
+    </div>
   )
 }
 
-export default page
+export default HomePage
